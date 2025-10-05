454460881f55cb01a676d7040aed2a1c903737e8
oseniabdulhaleem <oseniabdulhaleem@gmail.com>
2025-10-05 09:05:37 +0000

    made update to the delivery pipelines

--- iam.tf
+++ iam.tf
@@ -91,5 +91,5 @@ resource "google_cloud_run_service_iam_member" "allow_public_access_production" 
   role     = "roles/run.invoker"
   member   = "allUsers"
 
-  depends_on = [google_clouddeploy_delivery_pipeline, google_project_iam_member.terraform_sa_run_admin]
+  depends_on = [google_clouddeploy_delivery_pipeline.pipeline, google_project_iam_member.terraform_sa_run_admin]
 }
